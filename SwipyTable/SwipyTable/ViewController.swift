38154be1f467e3925f37a5112c574cf88cd70de6
Anant Vijay <anant.vijay@mindtree.com>
2019-12-17 21:29:51 +0530

    Initial Project Setup

---
+++ SwipyTable/SwipyTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipyTable
+//
+//  Created by Anant Vijay on 17/12/19.
+//  Copyright © 2019 Anant Vijay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
